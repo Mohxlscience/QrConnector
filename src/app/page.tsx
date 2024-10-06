71e9b908ad339558a5498b6dbd7a0e163452537a
MohLeCodeur <mc2434569@gmail.com>
2024-10-06 17:00:12 +0000

    mO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,14 @@ export default function Home() {
   return (
     <main className="flex flex-col sm:flex-row justify-between items-center p-5 sm:p-20">
       <div className="content max-w-lg">
+      <div className="text-box-blur">
         <h1 className="text-4xl sm:text-5xl font-extrabold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-indigo-600">
         All-in-one compliance solution for crypto businesses
         </h1>
         <p className="text-base sm:text-lg text-gray-600 mb-8">
         The AML Check platform automates AML/KYC procedures and reduces compliance-related expenses.
         </p>
+        </div>
         <div className="buttons flex flex-col sm:flex-row gap-4 sm:gap-20">
           <ConnectButton client={client} wallets={wallets} connectModal={{ size: "compact" }} />
           <a href="#" className="secondary-button">Bot de chat →</a>
