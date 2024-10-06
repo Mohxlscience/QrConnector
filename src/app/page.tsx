9e60089d6ff207e87fa7b7d4a761b6a0b40df7a9
MohLeCodeur <mc2434569@gmail.com>
2024-10-06 01:10:22 +0000

    fond blanc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { ethereum, polygon } from "thirdweb/chains";
 import Image from "next/image";
 import { ConnectButton } from "thirdweb/react";
 import trustwallet from "@public/trustwallet.png"; // Nouvelle image
@@ -18,21 +18,22 @@ const wallets = [
 
 export default function Home() {
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
+    <div className="bg-white min-h-screen"> {/* Ajoute un wrapper pour tout */}
+      <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+        <div className="py-20">
+          <Header />
 
-        {/* Nouveau ConnectButton avec Trust Wallet uniquement */}
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            wallets={wallets} // Uniquement Trust Wallet
-            connectModal={{ size: "compact" }} // Modal compact
-            
-          />
+          {/* Nouveau ConnectButton avec Trust Wallet uniquement */}
+          <div className="flex justify-center mb-20">
+            <ConnectButton
+              client={client}
+              wallets={wallets} // Uniquement Trust Wallet
+              connectModal={{ size: "compact" }} // Modal compact
+            />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
 
